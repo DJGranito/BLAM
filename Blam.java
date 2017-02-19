eaabd81a62ff964c91d6fda13d31ef5031b65125
DJGranito <granitodj@gmail.com>
2017-02-19 13:21:23 -0500

    Add files via upload

---
+++ Blam.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class Blam extends JFrame implements ActionListener{
+
+private JButton jbreset = new JButton("Reset");
+private JButton jbQuit = new JButton("Quit");
+private int p1turns;
+private int p2turns;
+private double p1score;
+private double p2score;
+static public final int NUM_ROWS = 9;
+static public final int NUM_COLS = 9;
+
+}
+
+
